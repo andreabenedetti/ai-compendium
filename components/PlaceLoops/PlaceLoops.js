48e6bd3117e06492a636813d984dc4200c3b2ff5
andrea <benedetti.a93@gmail.com>
2023-12-27 10:01:37 +0100

    update

--- components/PlaceLoops/PlaceLoops.js
+++ components/PlaceLoops/PlaceLoops.js
@@ -4,7 +4,18 @@ import { Col, Row } from "react-bootstrap";
 import * as d3 from "d3";
 
 export default function PlaceLoops( cards ) {
-    const data = cards.cards.sort((a, b) => d3.ascending(a.title, b.title));
+
+    const data = cards.cards;
+
+    data.forEach(d => {
+        d.humanCount = d.loop.filter(e => e.actor === "human").length;
+        d.algoCount = d.loop.filter(e => e.actor === "algorithm").length;
+    });
+
+    data.sort((a, b) => d3.descending(a.humanCount, b.humanCount) || d3.ascending(a.algoCount, b.algoCount));
+
+    console.log(data);
+
     return (
         <>        
         <Row xs={1} md={3} lg={3} className="g-2">
